107322194c8c26c29fcfb256657891fe4d0d7257
Dror Pezo <Drorpezo1@gmail.com>
2018-11-23 00:02:47 +0200

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -814,7 +814,7 @@ function hello_user(){
 		button_continue_training.innerHTML = "continue training";
 		button_continue_training.setAttribute("class","btn btn-success")
 		button_continue_training.addEventListener("click",function(){
-			console.log(models_stats);
+			console.log(models_stats.models);
 			call_continue_training(i);
 		});
 		cell6.appendChild(button_continue_training);
