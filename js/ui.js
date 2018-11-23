e9d0545b2d48ef012d6a598a6535e2aef3c3f626
Dror Pezo <Drorpezo1@gmail.com>
2018-11-23 15:22:15 +0200

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -796,11 +796,12 @@ function hello_user(){
 		cell3.innerHTML = models_stats.models[i].date;
 		cell4.innerHTML = models_stats.models[i].accuracy; 
 		cell5.innerHTML = models_stats.models[i].loss;
-
+		var name = models_stats.models[i].model_name;
+		console.log(name);
 		var button_continue_training = document.createElement("button");
 		button_continue_training.innerHTML = "continue training";
-		button_continue_training.setAttribute("class","btn btn-success")
-		button_continue_training.setAttribute("id", models_stats.models[i].model_name);
+		button_continue_training.setAttribute("id", name);
+		button_continue_training.setAttribute("class","btn btn-success");
 		console.log($('button_continue_training').attr('id'));
 		button_continue_training.addEventListener("click",function(){
 			var m_name = $(this).attr('id');
