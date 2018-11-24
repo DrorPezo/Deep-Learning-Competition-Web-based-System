0f8a29468742a9c398d39c9f23eaa4f3e7076adf
Dror Pezo <Drorpezo1@gmail.com>
2018-11-24 17:07:01 +0200

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -805,7 +805,7 @@ function hello_user(){
 		button_continue_training.setAttribute("class","btn btn-success");
 		button_array.push(button_continue_training);
 		button_array[i].addEventListener("click",function(){
-			var m_name = button_array[i].id;
+			var m_name = button_array[i];
 			console.log(m_name);
 			localStorage.setItem("model_name" ,m_name);
 			//window.location = "continue_training.html";
