358c1df7429e197adb84b8febbc199430f4703c6
Dror Pezo <Drorpezo1@gmail.com>
2018-11-29 10:27:30 +0200

    Update train.js

--- js/train.js
+++ js/train.js
@@ -408,9 +408,7 @@ async function Load_model() {
 	var bucket_name =  get_current_user();
 	var model_files=JSON.parse(get_model(bucket_name, model_n));
 	var pre_trained_model = await tf.loadModel(model_files[0]);
-	console.log(pre_trained_model.inputs);
-	console.log(pre_trained_model.output);
-	model = tf.model({inputs: pre_trained_model.inputs, outputs: last_layer.outputs});
+	model = tf.model({inputs: pre_trained_model.inputs, outputs: pre_trained_model.layers[2].output});
 	console.log(model);
 
 	document.getElementById('username_space').innerHTML=localStorage.getItem('user');
